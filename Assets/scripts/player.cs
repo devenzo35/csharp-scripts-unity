e306f127b81fcf4cfec7e7ea5209cf08d5598d91
Enzo <enzocuellar12@gmail.com>
2021-09-04 10:10:09 -0300

    add jump to player game object

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space")){
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+        }
+    }
+}
+ 
